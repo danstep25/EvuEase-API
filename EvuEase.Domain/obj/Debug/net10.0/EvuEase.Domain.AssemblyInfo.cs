8d7d9e54f9d981cac784da7ffc3521f827d0ce32
unonymous <uno.anonymous.uno@gmail.com>
2026-02-15 22:13:42 +0800

    added fix

--- EvuEase.Domain/obj/Debug/net10.0/EvuEase.Domain.AssemblyInfo.cs
+++ EvuEase.Domain/obj/Debug/net10.0/EvuEase.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvuEase.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebd1da81b95480d6eed4b020e53cf68946df136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d42d00ac468327060573113f453ee0b658227cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvuEase.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvuEase.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
