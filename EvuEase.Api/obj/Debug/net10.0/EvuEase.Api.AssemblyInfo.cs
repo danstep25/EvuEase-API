399c0b60564fc50b2b02ea75ecc65ebc40f709e9
unonymous <uno.anonymous.uno@gmail.com>
2026-04-06 22:45:08 +0800

    updated Registrar

--- EvuEase.Api/obj/Debug/net10.0/EvuEase.Api.AssemblyInfo.cs
+++ EvuEase.Api/obj/Debug/net10.0/EvuEase.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvuEase.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c850a27bd4f20a8cc6ac97c174697900b5f6ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec6dbb9c97515cd46a395a5b59d243b2fe6d35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvuEase.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvuEase.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
